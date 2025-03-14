924a07f66b738c6055b5f898997ffd4cbc93ca4f
ana sales <a.oliveira4@iesb.edu.br>
2025-03-13 21:36:34 -0300

    atualizando pratica03

---
+++ praticas/pratica03/App.js
@@ -0,0 +1,10 @@
+import ListScreen from "./Screens/ListScreen";
+import GalleryScreen from "./Screens/GalleryScreen";
+import HomeScreen from "./Screens/HomeScreen";
+import ProfileScreen from "./Screens/ProfileScreen";
+
+function App() {
+  return <ProfileScreen/>;
+}
+
+export default App;
